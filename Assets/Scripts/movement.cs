4eb1e64e9bb7ad161ac2ab7c42acb52958254351
Oholeiro <123236521+Q-Vortex@users.noreply.github.com>
2025-01-16 15:12:24 +0200

    Sumarry

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movingFunc : MonoBehaviour
+public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
