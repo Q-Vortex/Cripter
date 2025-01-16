c2844d0301a8e6e6fe9f8f3d7eb8b059d1779ad3
Skromn1k_ <skromn12k@gmail.com>
2025-01-16 20:01:19 +0200

    update

--- Assets/Scripts/DragMovement.cs
+++ Assets/Scripts/DragMovement.cs
@@ -3,5 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
+
+public class DragMovement : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+            Debug.Log(startPoint);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+
+        
+        
+        
+        }
+    }
 
 
+}
