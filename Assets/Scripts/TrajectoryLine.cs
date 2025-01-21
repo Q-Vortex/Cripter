1d8f3442d8b689e8be13b0e00aebb8a674fc0767
Skromn1k_ <skromn12k@gmail.com>
2025-01-21 20:20:42 +0200

    update

---
+++ Assets/Scripts/TrajectoryLine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class TrajectoryLine
+{
+    internal void RenderLine(Vector3 startPoint, Vector3 currentPoint)
+    {
+        throw new NotImplementedException();
+    }
+}
